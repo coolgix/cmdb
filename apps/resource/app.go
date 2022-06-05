7ea297430bc5f3d4c23028ab7d113b3d0a9a4c8f
coolgix <693059176@qq.com>
2022-06-05 22:10:47 +0800

    添加对sql的实现类

--- apps/resource/app.go
+++ apps/resource/app.go
@@ -3,3 +3,10 @@ package resource
 const (
 	AppName = "resource"
 )
+
+// HasTag 扩展SearchRequest 给他一个方法HasTag，让他返回一个当前的查询参数到底有没有带着tag来做一个查询
+//判断下长度是不是大于0，如果大于0就可以做过滤，如果等于0就没有tag
+func (r *SearchRequest) HasTag() bool {
+	return len(r.Tags) > 0
+
+}
--- apps/resource/impl/resource.go
+++ apps/resource/impl/resource.go
@@ -12,7 +12,28 @@ import (
 
 func (s *.service) Search(ctx context.Context, req *resource.SearchRequest) (
 	*resource.ResourceSet, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Search not implemented")
+	//sql 是一个模版，到底是左链接还是右链接，取决于我们是否需要关联tag表
+	// left join 是先扫描左表，right join 先扫描右表,当有tag 过滤，需要关联右表，可以以右表为准
+	//  如果扫描tag的成本比扫描resource的表的成本低，我们就用right join
+	//需要判断请求有没有传tag的条件给我们
+
+	join := "LEFT"
+	if req.Hastag(){
+		join = "RIGHT"
+
+	}
+	//构建sql具体使用那种方法
+	query := sqlbuilder.NewQuery(fmt.Sprintf(sqlQueryResource,join))
+
+	// 构建过滤条件
+	//关键字语句
+	if req.Keywords !=  "" {
+		//补充sql语句的条件
+		query.Where("")
+
+	}
+
+	return nil, nil
 }
 
 func (s *.service) QueryTag(ctx context.Context, req *resource.QueryTagRequest) (
